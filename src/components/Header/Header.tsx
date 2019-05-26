ff3f0d6902234510053bdf7127cc8ca349438add
cllpse <thomas@eject.io>
2019-05-26 23:37:32 +0200

    Replace a-elements with Buttons in component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 
 import styles from "./styles.module.css";
+import Button from "../Button/Button";
+import Icon from "../Icon/Icon";
 
 export default class Header extends Component {
     render() {
         return (
             <header className={styles.header}>
-                <a href="/" title="Navigate to the front page" className={styles.logo}></a>
+                <div className={styles.logo}></div>
 
                 <nav className={styles.navigation}>
-                    <a href="/" title="View shopping cart" className={styles.cart}></a>
-                    <a href="/" title="Open navigation menu" className={styles.menu}></a>
+                    <Button icon={<Icon path="/assets/outline-shopping_cart-24px.svg" />} />
+                    <Button icon={<Icon path="/assets/outline-menu-24px.svg" />} />
                 </nav>
             </header>
         );
