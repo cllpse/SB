19a450b8a3ae450f73e7d5baeff54cde5004e906
cllpse <thomas@eject.io>
2019-05-26 23:28:39 +0200

    Implement component HeroSection

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,28 +1,58 @@
-import React, { Component, ReactChild } from "react";
+import React, { Component } from "react";
 
 import styles from "./styles.module.css";
+import Button, { ButtonType } from "../Button/Button";
+import Icon from "../Icon/Icon";
 
-export interface Props {
+export interface IProps {
     headerText: string;
-    button: ReactChild;
-    buttonClicked: any;
+    buttonText: string;
 }
 
-export default class HeroSection extends Component<Props, {}>
-{
-    /* constructor(props: Props)
-    {
-        super(props);
-    } */
+interface IState {
+    videoPlaying?: boolean
+}
 
-    render()
-    {
+export default class HeroSection extends Component<IProps, IState>
+{
+    constructor(props: IProps) {
+        super(props);
+
+        this.state = { videoPlaying: false };
+    }
+
+    private playVideo() {
+        this.setState({videoPlaying: !this.state.videoPlaying});
+    }
+
+    render() {
         return (
-            <div className={styles.hero}>
-                HERO
-                {this.props.headerText}
-                {this.props.button}
-            </div>
+            <section className={styles.hero}>
+
+                {!this.state.videoPlaying &&
+                    <div>
+                        <video autoPlay muted loop className={styles.video}>
+                            <source src="assets/Desktop_loop_min.mp4" type="video/mp4" />
+                        </video>
+
+                        <div className={styles.content}>
+                            <h1 className={styles.header}>{this.props.headerText}</h1>
+
+                            <Button type={ButtonType.Primary} label={this.props.buttonText} onClicked={this.playVideo.bind(this)}  />
+                        </div>
+                    </div>
+                }
+
+                {this.state.videoPlaying &&
+                    <div className={styles.embed}>
+                        <iframe width="100%" height="auto" src="https://www.youtube.com/embed/x-Yx1crUFFY?autoplay=1" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+                        <div className={styles.close}>
+                            <Button icon={<Icon path="assets/outline-close-24px.svg" />} onClicked={this.playVideo.bind(this)} />
+                        </div>
+                    </div>
+                }
+            </section>
         );
     }
 }
