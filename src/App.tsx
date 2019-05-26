a407be09611253b5ec128f51c63ce2e271261a8d
cllpse <thomas@eject.io>
2019-05-26 15:07:02 +0200

    Project now uses TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./App.module.css";
+
+const App: React.FC = () =>
+{
+    return (
+        <div className={styles.app}>&hellip;!</div>
+    );
+}
+
+export default App;
